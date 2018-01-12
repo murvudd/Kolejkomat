d01246408c1b1bdd24a272e4c1374fdd7fac6cc5
murvudd <oddajcieminick@gmail.com>
2018-01-12 20:18:33 +0100

    5

---
+++ Żaba/sr2test/sr2test/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace sr2test
+{
+    public class ChatHub : Hub
+    {
+
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+    }
+}
