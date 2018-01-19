06e0a29e5b49aa06318ab1f5620efd44b6856ce9
murvudd_PC <oddajcieminick@gmail.com>
2018-01-19 01:52:54 +0100

    2

---
+++ Żaba/WebAPI/WebAPI/Events.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI;
+
+namespace WebAPI
+{
+
+    //public interface IEvent
+    //{
+    //    Guid Id { get; }
+    //}
+
+    //public class AccountCreateEvent : IEvent
+    //{
+    //    public string FirstName { get; set; }
+    //    public string LastName { get; set; }
+    //    public string Mail { get; set; }
+    //    public Guid Id { get; set; }
+    //    public string Password { get; set; }
+
+    //    public AccountCreateEvent(Guid id, string password, string name, string lastname, string mail)
+    //    {
+    //        Id = id;
+    //        Password = password;
+    //        FirstName = name;
+    //        LastName = lastname;
+    //        Mail = mail;
+    //    }
+    //}
+
+
+
+
+}
