81fe1ff56d77041824cb39887a80d1fb050d9bc9
murvudd_PC <oddajcieminick@gmail.com>
2018-01-26 00:55:58 +0100

    nie wiem co tu jest

---
+++ EntityProject/EntityProject/Models/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EntityProject.Models
+{
+    public enum Privileges
+    {
+        User = 0,
+        PrivilagedUser = 1,
+        Admin = 2,
+    }
+
+    public interface IGuid
+    {
+        Guid Id { get; }
+    }
+
+
+    [Table("Person")]
+    public class Person : IGuid
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Password { get; set; }
+
+        public Privileges Privileges { get; set; }
+
+
+
+        public int? PositionInQueuePos { get; set; }
+
+        public virtual PositionInQueue PositionInQueues { get; set; }
+
+        //public Person()
+        //{
+        //    Id = Guid.NewGuid();
+        //}
+    }
+}
---
+++ Żaba/FinalTest/FinalApp/MainHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalApp
+{
+    public class MainHub: Hub
+    {
+    }
+}
