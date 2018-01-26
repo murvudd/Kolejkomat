982d4801a29521b874b21d78a523d6281aff1cc4
murvudd_PC <oddajcieminick@gmail.com>
2018-01-26 05:07:28 +0100

    1

--- Żaba/Testy/ConsoleClient/Program.cs
+++ Żaba/Testy/ConsoleClient/Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNet.SignalR.Client;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
 
 namespace ConsoleClient
 {
@@ -11,7 +13,57 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
-            HubConnection conn = new HubConnection("http://localhost:8080/signalr");
+
+            //Set connection
+            var connection = new HubConnection("http://kolejkomatapp2.azurewebsites.net/signalr/hubs");
+            //Make proxy to hub based on hub name on server
+            var myHub = connection.CreateHubProxy("mainHub");
+            //Start connection
+
+            connection.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+
+            myHub.Invoke<string>("logIn", new string[] { "login ", "mail" }).ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error calling send: {0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine(task.Result);
+                }
+            });
+
+            //myHub.On<string>("addNewMessageToPage", param =>
+            //{
+            //    Console.WriteLine(param);
+            //});
+            myHub.Invoke<string>("signIn", "mail", "Password", "Klejnot", "Nilu").Wait();
+
+            myHub.On<string>("accountCreated", param =>
+            {
+                Console.WriteLine(param);
+            });
+
+            //myHub.Invoke<string>("Hello").Wait();
+
+
+            Console.Read();
+            connection.Stop();
         }
     }
 }
